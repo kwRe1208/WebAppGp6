09e9327089e90ade0dc2f26cbf345e34f2dff236
vishalmalhan305 <135173838+vishalmalhan305@users.noreply.github.com>
2023-11-16 15:15:51 -0500

    adding config.env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 // backend/server.js
 const app = require("./app");
 const connectDatabase = require("./config/database");
+const dotenv=require('dotenv');
+dotenv.config({path:'backend/config/config.env'});
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
